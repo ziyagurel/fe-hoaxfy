08cfaf471d25483deb77ce7d5441321374b0c1b7
ziyagurel <ziyagurel55@hotmail.com>
2020-06-11 14:10:13 +0300

    UserSingUp page has been created.

---
+++ src/UserSingUpPage.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class UserSignupPage extends React.Component{
+    state = {
+        username : null,
+        displayName : null,
+        password : null,
+        passwordRepeat : null,
+    }
+
+    onChange = event =>{
+        const {value,name} = event.target;
+        this.setState({
+            [name] : value
+        });
+    };
+
+    render(){
+        return(
+            <form>
+                <h1>Sign Up</h1>
+                <div>
+                    <label>Username</label>
+                    <input name="username" type="text" onChange={this.onChange}/>
+                </div>
+                <div>
+                    <label>Display Name</label>
+                    <input name="displayName" type="text" onChange={this.onChange}/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input name="password" type="password" onChange={this.onChange}/>
+                </div>
+                <div>
+                    <label>Password Repeat</label>
+                    <input name="passwordRepeat" type="password" onChange={this.onChange}/>
+                </div>
+                <button>Sign Up</button>
+            </form>
+        );
+    }
+}
+
+export default UserSignupPage;
