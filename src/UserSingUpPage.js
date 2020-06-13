663032432488e43f8c43edab9ca851029478d6d0
ziyagurel <ziyagurel55@hotmail.com>
2020-06-13 13:43:49 +0300

    Activating the button according to the response

--- src/UserSingUpPage.js
+++ src/UserSingUpPage.js
@@ -7,6 +7,7 @@ class UserSignupPage extends React.Component{
         displayName : null,
         password : null,
         passwordRepeat : null,
+        pendingApiCall : false
     }
 
     onChange = event =>{
@@ -23,8 +24,14 @@ class UserSignupPage extends React.Component{
             username,
             displayName,
             password
-        }
+        };
+        this.setState({pendingApiCall : true});
         axios.post('/api/1.0/users', body)
+        .then((response) => {
+            this.setState({pendingApiCall:false});
+        }).catch(error=> {
+            this.setState({pendingApiCall : false})
+        });
     }
 
     render(){
@@ -49,7 +56,8 @@ class UserSignupPage extends React.Component{
                         <input className="form-control" name="passwordRepeat" type="password" onChange={this.onChange}/>
                     </div>
                     <div className="text-center">
-                        <button className="btn btn-primary" onClick={this.onClickSignUp}>Sign Up</button>
+                        <button className="btn btn-primary" onClick={this.onClickSignUp} disabled={this.state.pendingApiCall}>
+                        {this.state.pendingApiCall && <span className="spinner-border spinner-border-sm"></span>} Sign Up</button>
                     </div>
                 </form>
             </div>
