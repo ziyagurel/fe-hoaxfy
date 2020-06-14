20502cd509b4cd1abbebc9b75de4427ec55f2415
ziyagurel <ziyagurel55@hotmail.com>
2020-06-14 12:40:05 +0300

    Updates

---
+++ precache-manifest.1c9e3c6939815716891932eb9323810e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818481e4c5f3d42488dfd2f81b069c5c",
+    "url": "/fe-hoaxfy/index.html"
+  },
+  {
+    "revision": "8525d5d3cd2edd462576",
+    "url": "/fe-hoaxfy/static/css/main.85957db9.chunk.css"
+  },
+  {
+    "revision": "5cc0c9f8765bb68e8580",
+    "url": "/fe-hoaxfy/static/js/2.294e14ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fe-hoaxfy/static/js/2.294e14ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8525d5d3cd2edd462576",
+    "url": "/fe-hoaxfy/static/js/main.be206a58.chunk.js"
+  },
+  {
+    "revision": "8af66b77468e5c657b71",
+    "url": "/fe-hoaxfy/static/js/runtime-main.5be4e2d3.js"
+  }
+]);
